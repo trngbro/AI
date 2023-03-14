186b75e118e673b43187007dbf83a1f22822c70b
Phuc <phuctran12421@gmail.com>
2023-03-14 22:49:20 +0700

    getpath()

--- searchAgents.py
+++ searchAgents.py
@@ -9,32 +9,49 @@ class UCS():
         pq = PriorityQueue()
         pq.enqueue((0, src))
         expanded = []
+        parents = {str(src): -1}
         path = []
         
         while not pq.isEmpty():
             cur_w, cur = pq.dequeue()
             expanded.append(cur)
         
-            if cur == dst: return expanded, path
+            if cur == dst: 
+                path = self.getPath(src, dst, parents)
+                return expanded, path
             
             successors = map.successor(cur)
             for v in successors:
                 if v not in expanded and not pq.contain(v):
+                    parents[str(v)] = cur
                     pq.enqueue((cur_w + 1, v))
                 elif pq.contain(v) and pq.getPriority(v) > cur_w + 1:
                     pq.updatePriority(v, cur_w + 1)
-        
+                    parents[str(v)] = cur
+               
+
+        path = self.getPath(src, dst, parents)
         return expanded, path
     
     def getPath(self, src: list, dst: list, parents: dict):
         path = []
+        path2= []
         x = dst
         while x != -1:
             path.append(x)
-            x = parents[x]
+            x = parents[str(x)]
         path.reverse()
-        return path
+        
+        for i in range(0,len(path)-1):
+            if path[i][0] > path[i+1][0]: path2.append("N")
+            elif path[i][0] < path[i+1][0]: path2.append("S")
+            elif path[i][1] > path[i+1][1]: path2.append("W")
+            else: path2.append("E")
+        return path2
+    
 ucs = UCS()
 e,p = ucs.search(a, a.P, a.G)
-print(e)
 print(p)
+# av =[1,2]
+# ab = str(av)
+# print(int(ab[4])+int(ab[1]))
